87a8c794d43512447bcf730d0db25f16f88200e5
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-11-28 17:49:58 -0600

    Initial Commit

---
+++ CADisplayLinkCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CADisplayLinkCounter
+//
+//  Created by Charles Martin Reed on 11/28/18.
+//  Copyright © 2018 Charles Martin Reed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
